7c40b1a481ebda53d0178143cc4092a57343eeef
Shruti Mayank <shrutimayank1@gmail.com>
2021-12-15 14:01:14 +0530

    Responsive issue solved

--- components/Cards/card.js
+++ components/Cards/card.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 export const Cards = () => {
   return (
     <div>
-        <div class="container mx-auto py-10 grid grid-cols-3 gap-7">
+        <div class="container mx-auto px-10 sm:px-0 py-10 grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3">
             <div className="p-4 py-7 text-center bg-custom-blue text-white rounded shadow-xl shadow-black">
                 <h2 className="font-bold text-2xl">Secure storage</h2>
                 <p className="text-small pt-2">We store the vast majority of the digital assets in secure offline storage.</p>
