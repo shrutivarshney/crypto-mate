01737b245f505026dfe2b240ff02d1984b702ca4
Shruti Mayank <shrutimayank1@gmail.com>
2021-12-15 13:55:59 +0530

    Header and card compnent added

---
+++ components/Cards/card.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import { useState } from 'react';
+
+export const Cards = () => {
+  return (
+    <div>
+        <div class="container mx-auto py-10 grid grid-cols-3 gap-7">
+            <div className="p-4 py-7 text-center bg-custom-blue text-white rounded shadow-xl shadow-black">
+                <h2 className="font-bold text-2xl">Secure storage</h2>
+                <p className="text-small pt-2">We store the vast majority of the digital assets in secure offline storage.</p>
+            </div>
+            <div className="p-4 py-7 text-center bg-custom-blue text-white rounded shadow">
+                <h2 className="font-bold text-2xl">Protected by insurance</h2>
+                <p className="text-small pt-2">Coinbase maintains crypto insurance and all USD cash balances are covered by FDIC insurance, up to a maximum of $250,000.</p>
+            </div>
+            <div className="p-4 py-7 text-center bg-custom-blue text-white rounded shadow">
+                <h2 className="font-bold text-2xl">Industry best practices</h2>
+                <p className="text-small pt-2">Coinbase supports a variety of the most popular digital currencies.</p>
+            </div>
+        </div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif"
+      },
+
+      colors: {
+        'custom-blue' : '#061122',
+        'custom-orange' : '#F04024'
+      }
+    },
   },
   variants: {
     extend: {},
