391361950cfa597e7570e3a0a7ad039e98bcba36
Shruti Mayank <shrutimayank1@gmail.com>
2021-12-24 02:50:26 +0530

    Issues fixed

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
@@ -11,6 +10,5 @@ ReactDOM.render(
     </React.StrictMode>,
 	document.getElementById("root")
 )
-
 
 reportWebVitals()
